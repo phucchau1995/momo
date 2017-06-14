1573a9013d1b2f9371a3e99562d46f390f4484a5
NguyenDangPhuoc <dangphuocbk@gmail.com>
2017-06-14 13:59:54 +0700

    interface login

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,16 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBs4tCR5bcSIia04MD2HWIM1aE2BHKu-JI",
+    authDomain: "momo-c2b7e.firebaseapp.com",
+    databaseURL: "https://momo-c2b7e.firebaseio.com",
+    projectId: "momo-c2b7e",
+    storageBucket: "momo-c2b7e.appspot.com",
+    messagingSenderId: "454280533058"
+  };
+  firebase.initializeApp(config);
+</script>
